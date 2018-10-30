a765271b45ca8b100a602df45c5382f34c94b04c
szczesnym <m.szczesny@onet.eu>
2018-10-30 12:05:25 +0100

    Testing parameter

--- src/main/java/com/oerlemans/siemiatycze/balance/service/BalanceService.java
+++ src/main/java/com/oerlemans/siemiatycze/balance/service/BalanceService.java
@@ -5,6 +5,7 @@ import com.oerlemans.siemiatycze.balance.util.BalanceUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,9 @@ import java.util.Random;
 
 @Service
 public class BalanceService {
+    @Value("${application.testing}")
+    private int TESTING;
+
     private double balanceValue;
     private static BalanceService instance = null;
     private static final Logger LOGGER = LoggerFactory.getLogger(BalanceService.class);
@@ -44,16 +48,25 @@ public class BalanceService {
     }
 
     private void readScaleData() {
-        String serialStringValue = serialController.getAssciBuffer();
-        LOGGER.info("BalanceService.readScaleData->serial data:" + serialStringValue);
-        balanceValue = BalanceUtils.scaleResultAsdouble(serialStringValue);
+
+        //REAL
+        if(TESTING == 0) {
+            String serialStringValue = serialController.getAssciBuffer();
+            balanceValue = BalanceUtils.scaleResultAsdouble(serialStringValue);
+            LOGGER.info("BalanceService.readScaleData->serial data:" + serialStringValue);
+        }
+        //TESTIN
+        if(TESTING == 1) {
+            balanceValue = mockScaleData();
+            LOGGER.info("BalanceService.readScaleData->serial data:" + balanceValue);
+        }
     }
 
     private double mockScaleData() {
         Random generator = new Random();
         double tempValue = generator.nextDouble() * 10000;
         LOGGER.info("Scale data Mocked:" + tempValue);
-        return tempValue;
+        return (double)Math.round(tempValue * 100) / 100;
     }
 
 
